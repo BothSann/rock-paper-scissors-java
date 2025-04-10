97a7ab1b723b1e34272ea24ef98ab0229df81138
BothSann <boththann76@gmail.com>
2025-04-10 17:27:32 +0700

    Add win/tie tracking and display for RockPaperScissors game

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -5,6 +5,10 @@ public class RockPaperScissors {
     private final Scanner scanner = new Scanner(System.in);
     private final Random random = new Random();
 
+    private int playerWins = 0;
+    private int computerWins = 0;
+    private int ties = 0;
+
     private static final int ROCK = 0;
     private static final int PAPER = 1;
     private static final int SCISSORS = 2;
@@ -69,4 +73,47 @@ public class RockPaperScissors {
         return input.equals("3") || input.equals("scissors") || input.equals("s");
     }
 
+    private int getComputerChoice() {
+        return random.nextInt(3);
+    }
+
+    private void displayChoices(int playerChoice, int computerChoice) {
+        System.out.println("\n" + "=".repeat(20) );
+        System.out.println("You chose: " + choiceToString(playerChoice));
+        System.out.println("Computer chose: " + choiceToString(computerChoice));
+        System.out.println("=".repeat(20));
+    }
+
+    public String choiceToString(int choice) {
+        return switch(choice) {
+            case ROCK -> "Rock";
+            case PAPER -> "Paper";
+            case SCISSORS -> "Scissors";
+            default -> "UNKNOWN";
+        };
+    }
+
+    private void determineWinner(int playerChoice, int computerChoice) {
+
+    }
+
+    public boolean isTie (int playerChoice, int computerChoice) {
+        return playerChoice == computerChoice;
+    }
+
+    public void handleTie() {
+        System.out.println("It's a tie! No one wins.");
+        ties++;
+    }
+
+    public boolean isPlayerWin (int playerChoice, int computerChoice) {
+        return (playerChoice == ROCK && computerChoice == SCISSORS)  ||
+                (playerChoice == PAPER && computerChoice == ROCK) ||
+                (playerChoice == SCISSORS && computerChoice == PAPER);
+    }
+
+    public void handlePlayerWin() {
+        System.out.println("Congratulations! You win this round! \uD83C\uDF89");
+        playerWins++;
+    }
 }
