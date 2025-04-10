60f60f05bebb3db2206a5231469bbce1fdb484ae
BothSann <boththann76@gmail.com>
2025-04-10 16:40:37 +0700

    first commit

---
+++ src/RockPaperScissors.java
@@ -0,0 +1,14 @@
+public class RockPaperScissors {
+
+
+    public void displayWelcomeMessage() {
+        System.out.println("=".repeat(20));
+        System.out.println(" WELCOME TO ROCK PAPER SCISSORS ");
+        System.out.println("=".repeat(20));
+        System.out.println("\nGame Rules:");
+        System.out.println("- Rock crushes scissors");
+        System.out.println("- Scissors cuts paper");
+        System.out.println("- Paper covers rock");
+        System.out.println("\nLet's play!\n");
+    }
+}
